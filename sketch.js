19b5078fd5678ce1ea5509429a71ad8107023350
noordseoostende <voropayev1@mail.ru>
2022-11-25 20:10:01 +0100

    20:10

--- sketch.js
+++ sketch.js
@@ -9,19 +9,20 @@ function draw() {
 
   noFill()
   stroke(255)
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 50; i++) {
 
     var r = map(sin(frameCount / 2), -1, 1, 0, 255)
-    var g = map(i, 0, 20, 0, 255)
-    var b = map(cos(frameCount), -1, 1, 255, 0)
+    var g = map(i, 0, 50, 100, 200)
+    var b = map(cos(frameCount), -1, 1, 200, 100)
 
     stroke(r,g,b)
+    rotate(frameCount / 20)
     beginShape()
-    for (var j = 0; j < 360; j+= 10) {
-      var rad = i * 8
+    for (var j = 0; j < 360; j+= 160) {
+      var rad = i * 3
       var x = rad * cos(j)
       var y = rad * sin(j)
-      var z = sin(frameCount + 2 + i * 10) * 50
+      var z = sin(frameCount * 2 + i * 5) * 50
 
 
 
